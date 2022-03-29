e8eae45218c14fb77e7536b0573db0df3f9d8e9b
Nathália Lourenção <nathalia.lourencao@fcamara.com.br>
2022-03-29 17:52:11 -0300

    Conectando a API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => { 
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+    }
+
+    loadAll();
+  }, []);
+
+
+  return(
+    <div className="page"> 
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div key={key}>
+            {item.title}
+          </div>
+        ))}
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-anonymous-default-export */
+const API_KEY = '8124a56e205542f40e645f8137d419a9'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+const basicFetch = async(endpoint) =>{
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default{
+    getHomeList: async () =>{
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_network=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_network=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_network=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_network=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_network=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ];
+    }
+}
+
+
